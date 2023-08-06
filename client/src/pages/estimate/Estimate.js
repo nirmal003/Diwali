fc12a6362075c7d28b6f72b19732b4982453e580
Nirmal <nirmalraj1025@gmail.com>
2023-08-06 22:37:48 +0530

    first commit

--- client/src/pages/estimate/Estimate.js
+++ client/src/pages/estimate/Estimate.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./estimate.css";
 
 function Estimate() {
-  return <div className="cart_list_con">Estimate</div>;
+  return <div className="cart_list_con">werty</div>;
 }
 
 export default Estimate;
