6b1a1af3b46d9371f64e414e26f55bcef8e02c7c
Nirmal <nirmalraj1025@gmail.com>
2023-08-19 13:04:21 +0530

    first commit

--- client/src/components/invoice/firebase.js
+++ client/src/components/invoice/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDeyt8Fl_5X4-i1yKgRbzKOsJIJVudUj0g",
-  authDomain: "awesome-crackers.firebaseapp.com",
-  projectId: "awesome-crackers",
-  storageBucket: "awesome-crackers.appspot.com",
-  messagingSenderId: "185704730233",
-  appId: "1:185704730233:web:9dc29ddf056a9aa2278ea8",
-  measurementId: "G-M1DCFTCYPQ",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
